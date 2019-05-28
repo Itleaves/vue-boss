eafbfd424224ab764cd3e54fe81c134606da9789
Itleaves <13882041244@163.com>
2019-05-28 14:30:32 +0800

    tst

--- webCode/boss/src/api/getworklist.js
+++
@@ -1,8 +0,0 @@
-import fetch from '../utils/fetch'
-
-export function getWorklist(){
-    return fetch({
-        url:"/api/getworklist",
-        method:"get",
-    })
-}
--- webCode/boss/src/store/mutation-types.js
+++
@@ -1,4 +0,0 @@
-export const ADD_USER = 'ADD_USER';
-export const CHANGEREGSTATE = 'CHANGEREGSTATE';
-export const CHANGLONGIN = 'CHANGLONGIN';
-export const ADD_WORKLIST = 'ADD_WORKLIST';
